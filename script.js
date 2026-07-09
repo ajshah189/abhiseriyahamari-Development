c140964009452c5ceea02dc7853393d44022f6ee
Abhishek Shah <ajshah189@gmail.com>
2026-07-10 02:17:47 +0530

    commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 import App from "./src/app.js";
 
-App.start();
+console.log("1 - script.js loaded");
+
+try {
+    console.log("2 - calling App.start()");
+    App.start();
+    console.log("3 - App.start() returned");
+} catch (e) {
+    console.error("ERROR:", e);
+}
